2e95aad0209f8b6bb88dc5b0970da62ff315dfb1
nathanweinberg <nathan2@stwmd.net>
2019-06-09 19:20:45 -0400

    Add license boilerplate

--- interface.py
+++ interface.py
@@ -1,3 +1,18 @@
+'''
+ACE
+Copyright (C) 2019 Nathan Weinberg
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import sys
 import csv
 import yaml
@@ -378,4 +393,7 @@ if __name__ == "__main__":
 			print(err)
 	
 	else:
+		# license boilerplate
+		print("GroceryHelper Copyright (C) 2019 Nathan Weinberg\nThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; type `show c' for details.\n")
+		
 		main()
